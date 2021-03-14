ceb710d89dcb17a891cef9f7aee39f7042fec504
yogaaaaaaa <yogaadhi@icloud.com>
2021-03-14 15:00:59 +0700

    add js functional 2

--- app.js
+++ app.js
@@ -12,4 +12,29 @@ function addTodo(event){
 
     //prevent event from submitting
     event.preventDefault()
+
+    //todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"> </i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"> </i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append todo list
+    todoList.appendChild(todoDiv)
 }
